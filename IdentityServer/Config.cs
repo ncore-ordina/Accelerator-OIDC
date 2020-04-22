6ce135d9d2b615571d0e8b663a7ee226eb3938a9
Wouter <5755057+woeterman94@users.noreply.github.com>
2020-04-22 10:02:29 +0200

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+
+new Client {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    Enabled = true,
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RequireConsent = true,
+                    AllowRememberConsent = true,
+                    RedirectUris =
+                      new List<string> {"https://localhost:44302/signin-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+}
+
+
+
+            };
+
+    }
+}
